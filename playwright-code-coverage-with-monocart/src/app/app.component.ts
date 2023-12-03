9ef3a22590f057bc9d1bb275de6a8ea4b1639325
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-12-03 21:36:27 +0000

    playwright code coverage with monocart initial app

---
+++ playwright-code-coverage-with-monocart/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'playwright-code-coverage-with-monocart';
+
+  onPressMeClick(event: Event) {
+    const pointerEvent = event as PointerEvent;
+    if (pointerEvent.ctrlKey) {
+      alert("button pressed with ctrl key modifier");
+    } else {
+      alert("button pressed without ctrl key modifier");
+    }
+  }
+}
