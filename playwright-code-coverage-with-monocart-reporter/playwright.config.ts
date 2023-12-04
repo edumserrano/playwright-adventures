3f5e7cc094c50246eef889680a2e6634bc07a908
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-12-04 18:10:06 +0000

    add sample tests

--- playwright-code-coverage-with-monocart-reporter/playwright.config.ts
+++ playwright-code-coverage-with-monocart-reporter/playwright.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
+const _webServerPort = 4200;
+const _webServerHost = '127.0.0.1';
+const _webServerUrl = `http://${_webServerHost}:${_webServerPort}`;
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,8 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL: _webServerUrl,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -36,42 +32,22 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: `npx ng serve --host=${_webServerHost} --port ${_webServerPort}`,
+    url: _webServerUrl,
+    reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    timeout: 1 * 60 * 1000, // 1 min
+  },
 });
--- playwright-code-coverage-with-monocart-reporter/tests/example.spec.ts
+++ playwright-code-coverage-with-monocart-reporter/tests/example.spec.ts
@@ -1,18 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('screenshot', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveScreenshot();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('press me without ctrl modifier', async ({ page }) => {
+  let dialogMessage = '';
+  page.on('dialog', dialog => {
+    dialogMessage = dialog.message();
+    dialog.accept()
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.goto('/');
+  const pressMeButton =  page.getByRole('button', { name: 'Press me' });
+  await pressMeButton.click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  expect(dialogMessage).toBe("button pressed without ctrl key modifier");
 });
+
+test('press me with ctrl modifier', async ({ page }) => {
+  let dialogMessage = '';
+  page.on('dialog', dialog => {
+    dialogMessage = dialog.message();
+    dialog.accept()
+  });
+
+  await page.goto('/');
+  const pressMeButton =  page.getByRole('button', { name: 'Press me' });
+  await pressMeButton.click({ modifiers: ['Control'] });
+
+  expect(dialogMessage).toBe("button pressed with ctrl key modifier");
+});
+
+
