8502c6a6586d5c85fbb1e5f4c0625b195438d287
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-12-09 13:18:27 +0000

    update code comments

--- demos/code-coverage-with-istanbul-via-webpack-babel-plugin/tests/example.spec.ts
+++ demos/code-coverage-with-istanbul-via-webpack-babel-plugin/tests/example.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from 'tests/_shared/app-fixtures';
 
 test('screenshot', async ({ page }) => {
+  // this relative navigation is possible because of the baseURL
+  // property that is configured int the playwright.config.ts
   await page.goto('/');
   await expect(page).toHaveScreenshot();
 });
 
+// comment this test and you'll see reduced code coverage on the
+// src/app/app.component.ts file
 test('press me without ctrl modifier', async ({ page }) => {
   let dialogMessage = '';
   page.on('dialog', dialog => {
@@ -19,6 +23,8 @@ test('press me without ctrl modifier', async ({ page }) => {
   expect(dialogMessage).toBe("button pressed without ctrl key modifier");
 });
 
+// comment this test and you'll see reduced code coverage on the
+// src/app/app.component.ts file
 test('press me with ctrl modifier', async ({ page }) => {
   let dialogMessage = '';
   page.on('dialog', dialog => {
