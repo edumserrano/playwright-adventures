c1e66886fd1c879037d51290c85c8f2d1d8ff0cb
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-12-20 14:37:46 +0000

    update main README regarding fixtures demo

--- demos/fixtures/tests/_shared/fixtures/playwright-project-info.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { PlaywrightProjectName } from 'playwright.config';
-
-export async function setupPlaywrightProjectInfo(
-  use: (playwrightProjectName: PlaywrightProjectName) => Promise<void>
-): Promise<void> {
-  const projectName = test.info().project.name as PlaywrightProjectName;
-  await use(projectName);
-}
