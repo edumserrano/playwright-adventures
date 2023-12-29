e4483c460379ffa3140ff515fa6997df5d7d55a3
Eduardo Serrano <eduardomserrano@outlook.com>
2023-12-29 18:26:18 +0000

    update monocart reporter options comments and remove console.logs

--- demos/monocart-reporter/playwright.monocart-reporter.ts
+++ demos/monocart-reporter/playwright.monocart-reporter.ts
@@ -5,8 +5,8 @@ export function getMonocartReporterOptions(testResultsDir: string): MonocartRepo
   const monocartOptions: MonocartReporterOptions = {
     name: "playwright monocart-reporter demo",
     outputFile: path.resolve(testResultsDir, "monocart-report.html"),
-    /* See https://github.com/cenfun/monocart-reporter#style-tags */
     tags: {
+      // See https://github.com/cenfun/monocart-reporter#style-tags
       "press-me-button": {
         background: "#0066cc",
       },
@@ -20,8 +20,17 @@ export function getMonocartReporterOptions(testResultsDir: string): MonocartRepo
         background: "#009933",
       },
     },
-    /* See https://github.com/cenfun/monocart-reporter#custom-columns */
     columns: (defaultColumns: any) => {
+      // See https://github.com/cenfun/monocart-reporter#custom-columns
+      //
+      // Need to define a custom column that will be used to map the data collected from the visitor
+      // function defined below.
+      // Because the visitor function is collectng test comments and the test comments only contain
+      // the @description comment item, I only need to define a single column with id 'description'.
+      //
+      // If the tests used multiple comment items, such as @description and @owner, then I would have
+      // to also define a custom column with id of 'owner' to map its values to the report.
+
       const descriptionColumn = defaultColumns.find((column: any) => column.id === "description");
       if (!descriptionColumn) {
         const index = defaultColumns.findIndex((column: any) => column.id === "duration");
@@ -38,8 +47,23 @@ export function getMonocartReporterOptions(testResultsDir: string): MonocartRepo
         });
       }
     },
-    /* See https://github.com/cenfun/monocart-reporter#collect-data-from-the-comments */
     visitor: (data: any, metadata: any, collect: any) => {
+      // See https://github.com/cenfun/monocart-reporter#collect-data-from-the-comments
+      //
+      // This implementation of the visitor function will analyze all test comments and
+      // collect comment items that start with '@'.
+      //
+      // The tests on this demo app only have a single comment item: '@description'. Therefore,
+      // this function will collect the comments from '@description' and save them in the data
+      // object, with the key 'description'.
+      // This means that the 'data.description' can be accessed in other parts of the report,
+      // such as by custom columns. Custom columns can pick up their values from the data
+      // object. For instance, defining a custom column with id of 'description' will pick up
+      // the values of the test comments that use the @description comment item.
+      //
+      // You can define multiple comment items in a single comment. For instance, you could
+      // define @description and @owner and have those two values added to the data object.
+
       const parserOptions = {
         sourceType: "module",
         plugins: ["typescript"],
@@ -47,8 +71,6 @@ export function getMonocartReporterOptions(testResultsDir: string): MonocartRepo
       const comments = collect.comments(parserOptions);
       if (comments && Object.keys(comments).length > 0) {
         Object.assign(data, comments);
-        console.log(comments);
-        console.log(data);
       }
     },
   };
