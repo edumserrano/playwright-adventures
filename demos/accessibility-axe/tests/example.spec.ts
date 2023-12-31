b6ef60190a0efe87ad77129ffb34ee28500975ea
Eduardo Serrano <eduardomserrano@outlook.com>
2023-12-31 13:28:14 +0000

    add async suffix to function name

--- demos/accessibility-axe/tests/example.spec.ts
+++ demos/accessibility-axe/tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, TestInfo } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 import { AxeResults } from "axe-core";
 
-async function attachAxeResults(
+async function attachAxeResultsAsync(
   testInfo: TestInfo,
   axeResults: AxeResults,
 ): Promise<void> {
@@ -21,7 +21,7 @@ test("a11y", async ({ page }, testInfo) => {
   const axeResults = await new AxeBuilder({ page }).analyze();
 
   // export scan results as a test attachment
-  await attachAxeResults(testInfo, axeResults);
+  await attachAxeResultsAsync(testInfo, axeResults);
 
   // verify that there are no violations in the returned scan results
   expect(axeResults.violations).toEqual([]);
@@ -38,7 +38,7 @@ test("a11y with exclusions", async ({ page }, testInfo) => {
     .analyze();
 
   // export scan results as a test attachment
-  await attachAxeResults(testInfo, axeResults);
+  await attachAxeResultsAsync(testInfo, axeResults);
 
   // verify that there are no violations in the returned scan results
   expect(axeResults.violations).toEqual([]);
