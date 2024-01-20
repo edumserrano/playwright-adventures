fb3544c825ac6dbd6aaed8ecc295628daf663a71
Eduardo Serrano <eduardomserrano@outlook.com>
2024-01-20 13:33:08 +0000

    Review and add docs for the accessibility-lighthouse demo

--- demos/accessibility-lighthouse/tests/lighthouse.spec.ts
+++ demos/accessibility-lighthouse/tests/lighthouse.spec.ts
@@ -1,39 +1,24 @@
-import { Page } from "@playwright/test";
 import { attachAuditReport } from "monocart-reporter";
 import { expect, test } from "tests/lighthouse-fixtures";
 import {
-  LighthouseAudit,
   LighthouseAuditOptions,
-  LighthouseResult,
+  lighthouseAuditAsync,
 } from "tests/lighthouse-audit";
 
-async function lighthouseAuditAsync(
-  page: Page,
-  remoteDebuggingPort: number,
-): Promise<LighthouseResult> {
-  const viewportSize = page.viewportSize();
-  if (!viewportSize) {
-    throw new Error("page.viewportSize cannot be null");
-  }
-
-  const lighthouseAudit = new LighthouseAudit();
-  const lighthouseAuditOptions: LighthouseAuditOptions = {
-    url: page.url(),
-    remoteDebuggingPort: remoteDebuggingPort,
-    viewportHeight: viewportSize.height,
-    viewportWidth: viewportSize.width,
-  };
-  return await lighthouseAudit.runAsync(lighthouseAuditOptions);
-}
-
-test("a11y", async ({ page, remoteDebuggingPort }, testInfo) => {
-  await page.goto("/");
-
+test("a11y", async ({ remoteDebuggingPort, viewport, baseURL }) => {
   const { categoriesResult, runnerResult } =
     await test.step("lighthouse audit", () => {
-      return lighthouseAuditAsync(page, remoteDebuggingPort);
+      // it's safe to use the ! operator in baseURL! and viewport! below
+      // because we check that these have a value in the autoBrowser
+      // fixture at /tests/lighthouse-fixtures.ts
+      const options: LighthouseAuditOptions = {
+        remoteDebuggingPort: remoteDebuggingPort,
+        url: baseURL!,
+        viewportHeight: viewport!.height,
+        viewportWidth: viewport!.width,
+      };
+      return lighthouseAuditAsync(options);
     });
-
   await attachAuditReport(runnerResult, test.info());
 
   expect(categoriesResult.performanceScore).toBeGreaterThanOrEqual(80);
